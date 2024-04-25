db18f2f16423388983521a209aad37c6e7a16047
Joel Warwick <60834182+jwarwick03@users.noreply.github.com>
2024-04-25 08:02:15 +0000

    ready for presentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,3 +224,19 @@ async function calculate() {
         }
     );
     }
+
+    $(document).ready(function() {
+
+        $('.fade').slick({
+          dots: true,
+          infinite: true,
+          speed: 500,
+          fade: true,
+          slide: 'div',
+          cssEase: 'linear',
+          autoplay: true,
+          autoplaySpeed: 2000
+        });
+    
+    
+    });
